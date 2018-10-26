ee071b6ab37bf78b3cd2dfb27946425284b09125
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-10-26 16:44:49 -0400

    added components and dependencies

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import  EventDashboard from '../../features/event/EventDashboard/EventDashboard'
 
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className='app'>
         <h1>Ad-Vents</h1>
+        <EventDashboard/>
       </div>
     );
   }
