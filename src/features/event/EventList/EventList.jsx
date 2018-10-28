08effc1b0d62ebea53a672991727f5f278f543f5
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-10-28 14:51:10 -0400

    added form, modified dashboard, attendee, listitem, list

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import EventListItem from './EventListItem';
+//import EventDashboard from '../event/EventDashboard/EventDashboard';
 
 class EventList extends Component {
   render() {
+    const {events} = this.props;
     return (
       <div>
         <h1>Event List </h1>
-        <EventListItem/>
-        <EventListItem/>
-        <EventListItem/>
+        {events.map((event) => (
+          <EventListItem key={event.id} event={event}/>
+        ))}
       </div>
     )
   }
