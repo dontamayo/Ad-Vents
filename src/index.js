3503b571a75ff7c4580d49fdf8ebba07ff0ff58b
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-10-31 00:42:23 -0400

    so much learning! I can't stop and it's already 12:42am

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css';
-import './index.css';
-import App from './app/layout/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+import App from "./app/layout/App";
+import * as serviceWorker from "./serviceWorker";
 
-const rootEL = document.getElementById('root');
+const rootEL = document.getElementById("root");
 
 let render = () => {
-    ReactDOM.render(<App />, rootEL)
-}
-if (module.hot){
-    module.hot.accept('./app/layout/App', () => {
-        setTimeout(render)
-
-    })
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootEL
+  );
+};
+if (module.hot) {
+  module.hot.accept("./app/layout/App", () => {
+    setTimeout(render);
+  });
 }
 render();
-
 
 //ReactDOM.render();
 
