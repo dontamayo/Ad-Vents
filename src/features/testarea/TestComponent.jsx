9acc5e21eec9224078dad5c2f62a970cb392328b
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-11-02 10:44:57 -0400

    practicing more redux

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,19 +1,29 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import { Button } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { incrementCounter, decrementCounter } from './testActions'
 
 const mapState = (state) => ({
-    data: state.test.data
+  data: state.test.data
 })
-    
 
- class TestComponent extends Component {
+const actions = {
+  incrementCounter,
+  decrementCounter
+}
+
+class TestComponent extends Component {
   render() {
+    const {incrementCounter, decrementCounter, data} = this.props;
     return (
       <div>
-        <h1>Test Area</h1>
-        <h3>The Answer is: {this.props.data}</h3>
+        <h1>How many dogs I have? </h1>
+        <h3>`you have {data} dogs`</h3>
+        <Button onClick={incrementCounter} color='green' content='Increment' />
+        <Button onClick={decrementCounter} color='red' content='Decrement' />
       </div>
     )
   }
 }
-export default connect(mapState)(TestComponent);
+
+export default connect(mapState, actions)(TestComponent)
--- src/features/testarea/testReducer.js
+++ src/features/testarea/testReducer.js
@@ -1,18 +1,30 @@
-import { INCREMENT_COUNTER, DECREMENT_COUNTER } from "./testConstants";
+import { INCREMENT_COUNTER, DECREMENT_COUNTER } from './testConstants';
+import { createReducer } from '../../app/common/util/reducerUtil'
 
 const initialState = {
-    data: 300202
+  data: 43
+};
+
+const incrementCounter = (state, payload) => {
+  return { ...state, data: state.data + 1 };
 }
 
-const testReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case INCREMENT_COUNTER:
-        return {...state, data: state.data + 1 };
-        case DECREMENT_COUNTER:
-        return {...state, data: state.data - 1};
-        default:
-        return state;
-    }
+const decrementCounter = (state, payload) => {
+  return { ...state, data: state.data - 1 };
 }
 
-export default testReducer;
+// const testReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case INCREMENT_COUNTER:
+//       return { ...state, data: state.data + 1 };
+//     case DECREMENT_COUNTER:
+//       return { ...state, data: state.data - 1 };
+//     default:
+//       return state;
+//   }
+// };
+
+export default createReducer(initialState, {
+  [INCREMENT_COUNTER]: incrementCounter,
+  [DECREMENT_COUNTER]: decrementCounter
+});
