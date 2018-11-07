e4491c8a21b90265989532bb72a0518ba4a86f65
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-11-07 14:33:57 -0500

    added pictures, modified chat, heade,info,page,sidebar

--- src/features/event/EventDatailed/EventDetailedSidebar.jsx
+++ src/features/event/EventDatailed/EventDetailedSidebar.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import { Segment, List, Item, Label } from "semantic-ui-react";
 
-const EventDetailedSidebar = () => {
+const EventDetailedSidebar = ({ attendees }) => {
+  const isHost = false;
   return (
+    // s_8.6.4
     <div>
-      <h1>SIdebar</h1>
+      <Segment
+        textAlign="center"
+        style={{ border: "none" }}
+        attached="top"
+        secondary
+        inverted
+        color="teal"
+      >
+        {attendees && attendees.length} {attendees && attendees.length === 1 ? 'Person': 'People' } Going
+      </Segment>
+      <Segment attached>
+        <List relaxed divided>
+          {attendees &&
+            attendees.map(attendee => (
+              <Item key={attendee.id} style={{ position: "relative" }}>
+                {isHost &&
+                <Label
+                  style={{ position: "absolute" }}
+                  color="orange"
+                  ribbon="right">
+                  Host
+                </Label>}
+                <Item.Image size="tiny" src={attendee.photoURL} />
+                <Item.Content verticalAlign="middle">
+                  <Item.Header as="h3">
+                    <a>{attendee.name}</a>
+                  </Item.Header>
+                </Item.Content>
+              </Item>
+            ))}
+        </List>
+      </Segment>
     </div>
-  )
-}
+  );
+};
 
-export default EventDetailedSidebar
+export default EventDetailedSidebar;
